f2f9e5b7473a9c987fce3153002eb574adc41a1b
gregstuderoaf <gregory.studer@oneacrefund.org>
2017-11-06 15:55:40 +0300

    Make main into a module.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function main() {
-    sendReply("Hello World!");
-}
+globals.main = function() {
+    sendReply("globals.main()");
+};
+
+var notMain = function() {
+    sendReply("notMain()");
+};
+
+module.exports = {
+    notMain: notMain
+};
